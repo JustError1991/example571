7c408aeeb6ba0b827073bed2367c1828bba7b031
JustError <9162367422@mail.ru>
2021-04-10 16:33:17 +0300

    First commit branch1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Дополнительное задание");
+        System.out.println("Дополнительное задание branch1");
     }
 }
